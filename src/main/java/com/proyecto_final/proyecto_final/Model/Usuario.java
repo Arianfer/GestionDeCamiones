edddd1169e75d50e0089fe7cf7837d0b18775316
Agustin Christiansen <christiansenagustin@gmail.com>
2026-05-04 00:12:42 -0300

    Clase Usuario

--- src/main/java/com/proyecto_final/proyecto_final/Model/Usuario.java
+++ src/main/java/com/proyecto_final/proyecto_final/Model/Usuario.java
@@ -1,4 +1,32 @@
 package com.proyecto_final.proyecto_final.Model;
 
+import com.proyecto_final.proyecto_final.Enums.Rol;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rol rol;
 }
