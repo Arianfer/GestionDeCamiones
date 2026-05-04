344641d91d5bf44672c1f65e674ceb15d49be3fd
WalterDiaz12 <diazwalterjesus@gmail.com>
2026-05-04 10:32:56 -0300

    Creacion modelo Ruta

--- src/main/java/com/proyecto_final/proyecto_final/Model/Ruta.java
+++ src/main/java/com/proyecto_final/proyecto_final/Model/Ruta.java
@@ -1,4 +1,27 @@
 package com.proyecto_final.proyecto_final.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "ruta")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Ruta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRuta;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @OneToMany(mappedBy = "ruta", cascade = CascadeType.ALL)
+    private List<Zona> zonas;
 }
