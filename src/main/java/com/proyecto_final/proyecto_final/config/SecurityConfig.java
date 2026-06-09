3df0f7a9545c73feae2bb3c32cf3b00e19aee2cc
Arianfer <arianshaffer@gmail.com>
2026-06-09 09:41:08 -0300

    Arreglo de Bugs de Login

--- src/main/java/com/proyecto_final/proyecto_final/config/SecurityConfig.java
+++ src/main/java/com/proyecto_final/proyecto_final/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authRequest ->
                         authRequest
 
-                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/usuarios/**").hasAnyRole("ADMIN", "OFICINA")
 
                                 .requestMatchers(
                                         "/",
