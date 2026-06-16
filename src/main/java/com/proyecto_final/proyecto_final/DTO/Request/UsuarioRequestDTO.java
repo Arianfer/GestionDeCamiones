5e28aaa9416478ab53f4f1b455c571dc763f589b
WalterDiaz12 <diazwalterjesus@gmail.com>
2026-06-16 10:44:43 -0300

    Agregue dependencia de validaciones en pom.xml

--- src/main/java/com/proyecto_final/proyecto_final/DTO/Request/UsuarioRequestDTO.java
+++ src/main/java/com/proyecto_final/proyecto_final/DTO/Request/UsuarioRequestDTO.java
@@ -1,6 +1,8 @@
 package com.proyecto_final.proyecto_final.DTO.Request;
 
 import com.proyecto_final.proyecto_final.Enums.Rol;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UsuarioRequestDTO {
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     private String nombre;
+
     private String apellido;
     private String dni;
     private String email;
